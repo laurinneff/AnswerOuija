25df86f864bfb5a7a39516fd1ae9ed8defcb8d57
Laurin Neff <laurinneff@gmail.com>
2019-05-15 13:35:53 +0200

    Make development easier (external config file)

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const log = require('./log')
 const Snooper = require('reddit-snooper')
+const fs = require('fs')
+
+log('loading config.json...')
+const config = JSON.parse(fs.readFileSync('config.json'))
+log('config.json loaded')
 
 const snooper = new Snooper(
     {
         // credential information is not needed for snooper.watcher
-        username: 'AnswerOuija',
-        password: '',
-        app_id: '',
-        api_secret: '',
-        user_agent: 'AnswerOuija v1.0.0',
+        username: config.username,
+        password: config.password,
+        app_id: config.app_id,
+        api_secret: config.app_secret,
+        user_agent: config.user_agent,
         automatic_retries: true, // automatically handles condition when reddit says 'you are doing this too much'
         api_requests_per_minuite: 60 // api requests will be spread out in order to play nicely with Reddit
     })
 
-const fs = require('fs')
 
 log('loading words.txt...')
 var wordsRaw = fs.readFileSync('words.txt').toString()
@@ -22,7 +26,7 @@ delete wordsRaw
 log('words.txt loaded')
 
 log('Waiting for posts...')
-snooper.watcher.getPostWatcher('AskOuija')
+snooper.watcher.getPostWatcher(config.source_subreddit)
     .on('post', function (post) { // post will be a json object containing all post information
         log('got a post')
         const data = post.data
@@ -42,7 +46,7 @@ snooper.watcher.getPostWatcher('AskOuija')
                 //text: `This is from ${data.url}\nI just wasn't able to figure out how to crosspost with the API yet\nBeep boop, I'm a bot`,
                 kind: 'link',
                 url: data.url,
-                sr: 'AnswerOuija',
+                sr: config.post_subreddit,
             }, function (err, statusCode, data) {
                 if (!err) {
                     log('post submitted')
@@ -53,7 +57,7 @@ snooper.watcher.getPostWatcher('AskOuija')
         else {
             log('title doesn\'t contain underscores')
             const word = words[Math.floor(Math.random() * (words.length - 1)) + 1];
-            log('AnswerOuija says:', word)
+            log('AnswerOuija says', word)
             log('submitting post')
             snooper.api.post("/api/submit", {
                 api_type: "json",
@@ -61,7 +65,7 @@ snooper.watcher.getPostWatcher('AskOuija')
                 //text: `This is from ${data.url}\nI just wasn't able to figure out how to crosspost with the API yet\nBeep boop, I'm a bot`,
                 kind: 'link',
                 url: data.url,
-                sr: 'AnswerOuija',
+                sr: config.post_subreddit,
             }, function (err, statusCode, data) {
                 if (!err) {
                     log('post submitted')
